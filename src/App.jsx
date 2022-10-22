c4da7be690c9728e3a42a59385156105306392c8
Pedro Ávila <pedro2003dressler@gmail.com>
2022-10-22 19:43:13 -0300

    feat: added changeinfo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Layout from "./pages/Layout.jsx";
 import Home from "./pages/Home.jsx";
-import Contact from "./pages/Contact.jsx";
-import AboutUs from "./pages/AboutUs.jsx";
 import LogIn from "./pages/LogIn.jsx";
 import NoPage from "./pages/NoPage.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChangeInfo from "./pages/ChangeInfo.jsx";
 import Logged from "./pages/Logged.jsx";
 import Chatroom from "./pages/Chatroom.jsx";
+import ChangePassword from "./pages/ChangePassword.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -15,12 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<AboutUs />} />
-          <Route path="contact" element={<Contact />} />
           <Route path="login" element={<LogIn />} />
           <Route path="logged" element={<Logged />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="chatroom" element={<Chatroom />} />
+          <Route path="change-info" element={<ChangeInfo />} />
+          <Route path='change-password' element={<ChangePassword />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
