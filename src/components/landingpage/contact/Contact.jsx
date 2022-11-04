6401ded521572195734c9ad676ac3eb5997b7ebf
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-11-04 00:02:41 -0300

    working email sys

--- src/components/landingpage/contact/Contact.jsx
+++ src/components/landingpage/contact/Contact.jsx
@@ -1,48 +1,66 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Section from './style.js' 
 import Title from '../../texts/Title'
+import styled from 'styled-components'
+import colors from '../../../global-styles/colors'
 import ContactLabel from './ContactLabel.jsx'
 import {AiOutlinePhone, AiOutlineMail} from 'react-icons/ai'
-import LandingButton from '../../buttons/LandingButton'
-import ULine from '../ULine'
-import mail from '../../../assets/icons/mail.png'
-import call from '../../../assets/icons/call.png'
-import face from '../../../assets/icons/face.png'
-import insta from '../../../assets/icons/insta.png'
-import link from '../../../assets/icons/linkedin.png'
+import { EmailAPI } from '../../../data/api/hooks/services/EmailService.ts'
 
-const Contact = ({id}) => {
+const Contact = ({ id }) => {
 
-  const [values, setValues] = useState({
-    name: '',
-    email: '',
-    title: '',
-    text: ''
-  });
-
-  const onChange = (e) => {
-    setValues({...values, [e.target.name]: e.target.value});
-  };
+  const sendEmail = () => {
+    const inputName = document.getElementById('inputName').value;
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputTitle = document.getElementById('inputTitle').value;
+    const inputText = document.getElementById('inputText').value;
+    const emailAPI = EmailAPI()
+    let dataTosend = JSON.stringify({
+        inputName,
+        inputEmail,
+        inputTitle,
+        inputText 
+    })
+    emailAPI.post('/mailOpinion/opinion',dataTosend)
+  }
 
   return (
   <Section id={id}>
     <div className="message-container">
       <Title size={1.4}>Entre em contato com a gente!</Title>
       <div className="input-container">
-        <input type="text" placeholder='Nome' onChange={onChange} />
-        <input type="email" placeholder='Email' onChange={onChange} />
-        <input type="text" placeholder='Assunto' onChange={onChange} />
-        <textarea cols="30" rows="10" placeholder='Mensagem' onChange={onChange}></textarea>
+        <input type="text" placeholder='Nome' id="inputName"/>
+        <input type="email" placeholder='Email' id="inputEmail"/>
+        <input type="text" placeholder='Assunto' id="inputTitle"/>
+        <textarea cols="30" rows="10" placeholder='Mensagem' id="inputText"></textarea>
       </div>
       <hr />
       <div className="contact-container">
-        <ContactLabel value='contato@techthemis.com' icon={<AiOutlineMail />}/>
+        <ContactLabel value='suporteconsultoriatechthemis@gmail.com' icon={<AiOutlineMail />}/>
         <ContactLabel value='(31) 3259-0984' icon={<AiOutlinePhone />}/>
       </div>
-      <LandingButton button type='submit'>Enviar</LandingButton>
+      <Button button type='submit' onClick={sendEmail}>Enviar</Button>
     </div>
   </Section>
  )
 }
 
+const Button = styled.button`
+  padding: 10px 25px;
+  background-color: ${colors.logoGreenOne};
+  border-radius: 5px;
+  border: 0;
+  color: white;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 0.9rem;
+  font-weight: normal;
+  letter-spacing: 1px;
+  text-shadow: 0 2px 3px rgba(0,0,0,.5);
+
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
 export default Contact
