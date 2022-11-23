7fc92c4732d7ab2795f6298c4506c8591a17a5ae
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-11-22 21:41:18 -0300

    fixing css

--- src/global-styles/neumorfismo.js
+++ src/global-styles/neumorfismo.js
@@ -2,9 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const Neumorfismo = createGlobalStyle`
   .neumorph{
-    box-shadow:
-      -3px -3px 10px rgba(94, 104, 121, .288),
-      3px 3px 10px rgba(0,0,0, .5);
+    
   }
 `;
 
