57cd2ddd6b543a50d44a2525bc25be2ca29fd003
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-11-22 21:30:18 -0300

    arrumando largura do texto

--- src/components/landingpage/welcome/HelloContent.jsx
+++ src/components/landingpage/welcome/HelloContent.jsx
@@ -20,7 +20,7 @@ const HelloContent = () => {
       <Title size={2}>Consultoria Tech Themis</Title>
       <div className='texts'>
         <Text width={90}>É complicado procurar advogados na internet, mas você já passou por problemas para achar um, que seja confiável e que faça um bom trabalho? Demorou bastante para resolver aquele B.O?</Text>
-        <Text width={90}>Pois bem, nós pensamos nesse problema e dedicamos a criar uma solução para ele! A <CTTSpan>Consultoria Tech Themis</CTTSpan> é uma empresa especializada em facilitar o seu contato com advogados de confiança. Oferecemos agiligade com os processos burocrativos, flexibilidade na hora de escolher o advogado perfeito e um atendimento 24/7. Venha conhecer a gente!</Text>
+        <Text width={70}>Pois bem, nós pensamos nesse problema e dedicamos a criar uma solução para ele! A <CTTSpan>Consultoria Tech Themis</CTTSpan> é uma empresa especializada em facilitar o seu contato com advogados de confiança. Oferecemos agiligade com os processos burocrativos, flexibilidade na hora de escolher o advogado perfeito e um atendimento 24/7. Venha conhecer a gente!</Text>
       </div>
       <div className="button-container">
         <LandingButton href='contact'>
